d5c7258a026bf20ebb44a3ab155632ab804cf9aa
Kurniawanajisr <kajii339@gmail.com>
2026-09-18 23:09:47 +0700

    Update App.js

--- Pemrograman Mobile/Pertemuan2/ptmn2/App.js
+++ Pemrograman Mobile/Pertemuan2/ptmn2/App.js
@@ -4,7 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={styles.title}>CURRICULUM VITAE</Text>
+
+      <Text style={styles.label}>Nama Lengkap</Text>
+      <Text style={styles.text}>Kurniawan Aji Saputra Romadhon</Text>
+
+      <Text style={styles.label}>NIM</Text>
+      <Text style={styles.text}>2488010027</Text>
+
+      <Text style={styles.label}>Asal Sekolah</Text>
+      <Text style={styles.text}>SMK Negeri 2 Kota Tangerang</Text>
+
+      <Text style={styles.label}>Cita-cita</Text>
+      <Text style={styles.text}>produser</Text>
+
+      <Text style={styles.label}>Rencana Menggapai Cita-cita</Text>
+      <Text style={styles.text}>
+      Saya ingin menjadi produser musik karena suka mendengarkan aransemen berbagai macam gendre lagu, cara saya mencapai cita-cita saya adalah mendengarkan lebih banyak lagu dan juga membuat aransemen lagu walaupum tidak pernah tapi saya yakin pasti saya bisa MERDEKA.
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +33,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 25,
     justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
+  }, 
+
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 15,
+  },
+
+  text: {
+    fontSize: 16,
+    marginTop: 5,
   },
 });
